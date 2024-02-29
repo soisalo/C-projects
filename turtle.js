4d016c96aebc7bb4d15e2f31756c79bd53cbc496
soisalo <70382578+soisalo@users.noreply.github.com>
2024-02-29 14:02:31 +0200

    Update turtle.js

--- turtle.js
+++ turtle.js
@@ -18,82 +18,188 @@ color([r,g,b])
 color("red")
 color("#ff0000")
 */
+const screen = new TurtleCanvas("canvas", 1200, 600);
+screen.setBackground("white");
 
+// Create a turtle object 
+const pointer = new Turtle(screen);
+pointer.setSpeed(2);
+pointer.setColor("blue"); // Set color of the turtle pointer 
 
-
-setSpeed(200);
-
-width(5);
-color("blue");
-forward(120);
-
-width(1);
-left(65);
-color("red");
-forward(150);
-
-goto(184, -208);
-width(2);
-right(100);
-color("green");
-forward(150);
-
-// rectagles which bounce off the side of the canvas
-function init_drops(n) {
-    var drops = new Array(n);
-    for (var i = 0; i < n; i++) {
-        var x = random(-150, 150);
-        var y = random(-150, 150);
-        var velocityX = random(-6, 6);
-        var velocityY = random(-6, 6);
-        var size = random(20, 300);
-        var width = random(1, 40);
-        var a = Math.random();
-        var r = random(0, 255);
-        var g = random(0, 255);
-        var b = random(0, 255);
-        drops[i] = {
-            x: x,
-            y: y,
-            velocityX: velocityX,
-            velocityY: velocityY,
-            size: size,
-            r: r,
-            g: g,
-            b: b,
-            width: width,
-            a: a
-        };
-    }
-    return drops;
+// Function to write "Eemil" with the turtle pointer 
+function write_eemil() {
+    pointer.moveTo(-200, 0); // Set starting position 
 }
 
-function rain(drops, n) {
-    clear();
-    for (var i = 0; i < n; i++) {
-        var d = drops[i];
-        colour(d.r, d.g, d.b, d.a);
-        width(d.width);
-        goto(d.x, d.y);
-        if (d.y < -150) {
-            d.velocityY = -d.velocityY;
-        } else if (d.y + d.size > 150 && d.velocityY > 0) {
-            d.velocityY = -d.velocityY;
-        }
-        if (d.x - d.width / 2 < -150) {
-            d.velocityX = -d.velocityX;
-        } else if (d.x + d.width / 2 > 150) {
-            d.velocityX = -d.velocityX;
-        }
-        forward(d.size);
-        d.y += d.velocityY;
-        d.x += d.velocityX;
-    }
+function write_E() {
+    pointer.turnLeft(90);
+    pointer.forward(100);
+    pointer.turnRight(90);
+    pointer.forward(50);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(40);
+    pointer.turnLeft(90);
+    pointer.forward(35);
+    pointer.turnLeft(90);
+    pointer.forward(40);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(40);
+    pointer.turnLeft(90);
+    pointer.forward(35);
+    pointer.turnLeft(90);
+    pointer.forward(40);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnLeft(90);
+    pointer.forward(20);
 }
 
-function demo(n) {
-    wrap(false);
-    hideTurtle();
-    drops = init_drops(n);
-    animate(function() { rain(drops, n) }, 100);
+function write_M() {
+    pointer.turnLeft(80);
+    pointer.forward(100);
+    pointer.turnRight(130);
+    pointer.forward(50);
+    pointer.turnLeft(100);
+    pointer.forward(50);
+    pointer.turnRight(130);
+    pointer.forward(100);
+    pointer.turnRight(100);
+    pointer.forward(10);
+    pointer.turnRight(80);
+    pointer.forward(75);
+    pointer.turnLeft(130);
+    pointer.forward(40);
+    pointer.turnRight(100);
+    pointer.forward(40);
+    pointer.turnLeft(130);
+    pointer.forward(75);
+    pointer.turnLeft(100);
+    pointer.forward(105);
 }
+
+function write_I() {
+    pointer.turnLeft(90);
+    pointer.forward(100);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(100);
+    pointer.turnLeft(90);
+}
+
+function write_L() {
+    pointer.forward(20);
+    pointer.turnLeft(90);
+    pointer.forward(100);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(90);
+    pointer.turnLeft(90);
+    pointer.forward(60);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(355);
+    pointer.turnRight(180);
+    pointer.forward(360);
+    pointer.turnLeft(90);
+    pointer.penUp();
+    pointer.forward(70);
+    pointer.penDown();
+}
+
+function write_heart() {
+    pointer.beginFill("red");
+    pointer.turnLeft(50);
+    pointer.forward(60);
+    pointer.circle(-30, 200);
+    pointer.setHeading(60);
+    pointer.circle(-30, 200);
+    pointer.forward(60);
+    pointer.endFill();
+    pointer.turnLeft(50);
+    pointer.penUp();
+    pointer.forward(70);
+    pointer.turnLeft(90);
+    pointer.forward(10);
+    pointer.penDown();
+    pointer.setColor("DeepPink");
+    pointer.forward(25);
+    pointer.turnLeft(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+}
+
+function write_S() {
+    pointer.forward(10);
+    pointer.circle(18, 160);
+    pointer.circle(-28, 170);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.circle(18, 170);
+    pointer.circle(-28, 160);
+    pointer.turnRight(180);
+    pointer.forward(45);
+}
+
+function write_U() {
+    pointer.forward(30);
+    pointer.turnLeft(180);
+    pointer.circle(-30, 90);
+    pointer.forward(70);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(70);
+    pointer.circle(20, 180);
+    pointer.forward(70);
+    pointer.turnRight(90);
+    pointer.forward(10);
+    pointer.turnRight(90);
+    pointer.forward(70);
+    pointer.circle(-30, 90);
+}
+
+function write_V() {
+    pointer.turnRight(180);
+    pointer.forward(80);
+    pointer.turnLeft(110);
+    pointer.forward(105);
+    pointer.turnRight(110);
+    pointer.forward(10);
+    pointer.turnRight(70);
+    pointer.forward(95);
+    pointer.turnLeft(140);
+    pointer.forward(95);
+    pointer.turnRight(70);
+    pointer.forward(10);
+    pointer.turnRight(110);
+    pointer.forward(105);
+    pointer.turnLeft(110);
+    pointer.forward(50);
+}
+
+// Main function 
+function main() {
+    write_eemil();
+    write_E();
+    write_E();
+    write_M();
+    write_I();
+    write_L();
+    write_heart();
+    write_S();
+    write_U();
+    write_V();
+    write_I();
+    pointer.turnLeft(180);
+    pointer.forward(270);
+}
+
+main();
